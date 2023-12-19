56264de2b51a8bf5bf248b9b44f6f909559a4489
Brandon Garcia <grandonleebarcia@gmail.com>
2023-12-19 15:01:18 -0800

    added section dividers

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
 import Image from 'next/image';
 import Logo from './assets/ple-essentials-favicon-black.png';
 import Logo2 from './assets/ple-essentials-high-resolution-logo-white-transparent.png';
-
 import Warehouse from './assets/warehouse.jpg';
+import './globals.css';
 export default function Home() {
+  function CustomButton({ children }) {
+    return (
+      <a
+        href="#_"
+        class="relative  items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
+      >
+        <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+        <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+        <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
+          {children}
+        </span>
+        <span class="absolute inset-0 border-2 border-white rounded-full"></span>
+      </a>
+    );
+  }
+
   return (
     <main>
       <section className="md:container md:mx-auto px-4 flex justify-between items-center">
@@ -20,17 +36,21 @@ export default function Home() {
         </div>
       </section>
       <section className="h-screen">
-        <div className="bg-gradient-to-r from-gray-700 via-gray-700 to-black h-2/3 w-full bg-cover relative flex justify-center items-center">
+        <div className="bg-gradient-to-r from-gray-700 via-gray-700 to-black h-full w-full bg-cover relative flex justify-center items-center ">
           <Image
             className="w-full h-full object-cover absolute mix-blend-overlay"
             src={Warehouse}
           ></Image>
-          <div className=" w-1/3">
+          <div className="flex flex-col gap-6 w-1/3">
             <div className="text-white text-8xl font-semibold uppercase ">
               mechanical, collision, accessory
             </div>
-            <div className="text-white text-4xl font-semibold  ">
+            <div className="text-white text-4xl font-semibold">
               a part selection that leads the industry.
+            </div>
+            <div className="flex w-1/2 justify-between">
+              <CustomButton>Learn More</CustomButton>
+              <CustomButton>Contact</CustomButton>
             </div>
           </div>
           <div className="w-1/3">
@@ -43,9 +63,41 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section>About Us</section>
-      <section>Our Services</section>
-      <section>Contact</section>
+      <section className="relative flex flex-col  min-h-[700px] bg-gray-700">
+        <div>About Us</div>
+      </section>
+      <section className="relative flex flex-col  min-h-[800px] ">
+        <div class="custom-shape-divider-top-1703023278">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+              class="shape-fill"
+            ></path>
+          </svg>
+        </div>
+        <div>Services</div>
+      </section>
+      <section className="relative flex flex-col  min-h-[700px] bg-gray-700">
+        <div class="custom-shape-divider-top-1703023426">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+              class="shape-fill"
+            ></path>
+          </svg>
+        </div>
+        <div>Contact</div>
+      </section>
     </main>
   );
 }
