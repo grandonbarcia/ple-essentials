0885d01ddbeca7ae53923186e3431d01a1c316e1
Brandon Garcia <grandonleebarcia@gmail.com>
2023-12-21 12:39:15 -0800

    added turbo image

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,21 @@ import Image from 'next/image';
 import Logo from './assets/ple-essentials-favicon-black.png';
 import Logo2 from './assets/ple-essentials-high-resolution-logo-white-transparent.png';
 import Warehouse from './assets/warehouse.jpg';
+import Turbo from './assets/turbo.jpg';
 import './globals.css';
 export default function Home() {
   function CustomButton({ children }) {
     return (
       <a
         href="#_"
-        class="relative  items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
+        className="relative  items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
       >
-        <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-        <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
-        <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
+        <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+        <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+        <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
           {children}
         </span>
-        <span class="absolute inset-0 border-2 border-white rounded-full"></span>
+        <span className="absolute inset-0 border-2 border-white rounded-full"></span>
       </a>
     );
   }
@@ -63,11 +64,29 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="relative flex flex-col  min-h-[700px] bg-gray-700">
-        <div>About Us</div>
+      <section className="relative flex justify-center items-center min-h-[700px] bg-gray-700">
+        <div className="w-1/3">
+          <Image src={Turbo}></Image>
+        </div>
+        <div className="w-1/3">
+          <div>About Us</div>
+          <div>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </div>
+        </div>
       </section>
       <section className="relative flex flex-col  min-h-[800px] ">
-        <div class="custom-shape-divider-top-1703023278">
+        <div className="custom-shape-divider-top-1703023278">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -76,14 +95,14 @@ export default function Home() {
           >
             <path
               d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
-              class="shape-fill"
+              className="shape-fill"
             ></path>
           </svg>
         </div>
         <div>Services</div>
       </section>
       <section className="relative flex flex-col  min-h-[700px] bg-gray-700">
-        <div class="custom-shape-divider-top-1703023426">
+        <div className="custom-shape-divider-top-1703023426">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -92,7 +111,7 @@ export default function Home() {
           >
             <path
               d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
-              class="shape-fill"
+              className="shape-fill"
             ></path>
           </svg>
         </div>
